1671f2793093647ab0e1a0b0dfdf7898acd1b555
iiutkarsh <iiutkarshsri@proton.me>
2025-04-06 08:36:48 +0530

    All the work done

--- H2S/settings.py
+++ H2S/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
--- H2S/urls.py
+++ H2S/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('h2sapp.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
