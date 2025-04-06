694d7c5e06b0216f1af2e295911be28f4e1c5388
iiutkarsh <iiutkarshsri@proton.me>
2025-04-06 08:06:38 +0530

    Fixed static files structure and collectstatic

--- H2S/settings.py
+++ H2S/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'h2sapp', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
