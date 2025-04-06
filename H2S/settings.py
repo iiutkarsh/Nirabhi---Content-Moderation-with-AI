9157a2561993815429ecb19c1024bea1f6e0d4ce
iiutkarsh <iiutkarshsri@proton.me>
2025-04-06 06:43:41 +0530

    error

--- H2S/settings.py
+++ H2S/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dut_c)bfc@36incwl5s(j5g)2jb#fump==45xrs#_l^m_q9ypy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
