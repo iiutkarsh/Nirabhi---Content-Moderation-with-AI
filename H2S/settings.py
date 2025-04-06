5428fb1397c205ce02b72e4ec0f602e3dcf45bf3
iiutkarsh <iiutkarshsri@proton.me>
2025-04-06 05:51:32 +0530

    deployment

--- H2S/settings.py
+++ H2S/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dut_c)bfc@36incwl5s(j5g)2jb#fump==45xrs#_l^m_q9ypy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
